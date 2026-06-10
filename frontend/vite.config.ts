fa07fefd05fbb3689d68441813c451c044b77cec
Yeshvikaa <yeshuprojects@gmail.com>
2026-06-10 12:08:14 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://mind-guard-bcc4.onrender.com',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'https://mind-guard-bcc4.onrender.com',
         changeOrigin: true
       }
     }
